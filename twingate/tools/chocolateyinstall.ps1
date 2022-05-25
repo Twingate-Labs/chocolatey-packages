e7857646bf6ffff4e1d1e4400db87f8702e47bb6
Emrul Islam <emrul@emrul.com>
2022-05-25 17:11:31 +0100

    Initial commit

---
+++ twingate/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = "https://binaries.twingate.com/client/windows/versions/1.0.18.11084/TwingateWindowsInstaller.msi"
+$checksum64 = '9e83fe5e8095784e83e7cfccc1b663e6ea299e443599dd51a3c621fed0a70059'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'twingate*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:ChocolateyPackageVersion).MsiInstall.log`" no_optional_updates=true"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ twingate/update.ps1
@@ -0,0 +1,26 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $package = [AUPackage]::new( $pwd )
+    $url = "https://corp.twingate.com/api/versions/windows"
+    $update_data = Invoke-RestMethod -Uri $url -Body @{app_version = $package.NuspecVersion}
+    $url     = $update_data.data.downloadable_url
+    $version = $($env:ChocolateyPackageVersion)
+    if ( (200 -eq $update_data.status) -and ($true -eq $update_data.data.auto_update) ) {
+        $url     = $url -replace '.exe$', '.msi'
+        $version = $url -split '/' | Select-Object -Last 1 -Skip 1
+        return @{ Version = $version; URL64 = $url }
+    }
+    return @{ Version = $package.NuspecVersion }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            '(^[$]url64\s*=\s*)(".*")'      = "`${1}`"$($Latest.URL64)`""
+            "(^[$]checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+Update-Package -ChecksumFor 64
